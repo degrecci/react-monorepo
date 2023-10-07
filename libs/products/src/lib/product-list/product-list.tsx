793f184760c19804afaae4fde4ab339f1e13d1ec
Henrique Degrecci <hdegrecci@gmail.com>
2023-10-07 19:06:15 -0300

    Test change affected

--- libs/products/src/lib/product-list/product-list.tsx
+++ libs/products/src/lib/product-list/product-list.tsx
@@ -7,6 +7,7 @@ export function ProductList(props: ProductListProps) {
   return (
     <Container>
       <h1>Welcome to ProductList!</h1>
+      <p>This is a change. 👋</p>
     </Container>
   );
 }
