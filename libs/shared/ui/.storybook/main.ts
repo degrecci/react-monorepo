495269387300b9a55040ae7cd4d5cefb152b72e8
Henrique Degrecci <hdegrecci@gmail.com>
2023-10-12 21:22:06 -0300

    Fix tests failing

--- libs/shared/ui/.storybook/main.ts
+++ libs/shared/ui/.storybook/main.ts
@@ -1,7 +1,6 @@
 import type { StorybookConfig } from '@storybook/react-vite';
-
-import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 import { mergeConfig } from 'vite';
+import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 
 const config: StorybookConfig = {
   stories: ['../src/lib/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
--- libs/shared/ui/src/index.ts
+++ libs/shared/ui/src/index.ts
@@ -1,2 +1,1 @@
 export * from './lib/container/container';
-export * from './lib/shared-ui';
